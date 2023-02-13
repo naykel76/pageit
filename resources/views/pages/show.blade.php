36248527ca7e19582bed6e40a145a0e3cb9b4726
Nathan <nathan@naykel.com.au>
2023-02-13 15:48:35 +1000

    remove lorem text

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -8,7 +8,9 @@
                 <h1 class="title tac">{{ $page->title }}</h1>
             @endunless
 
-            <p class="lead tac">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi harum magni quam officiis rem porro, exercitationem cumque ex.</p>
+            @if($page->intro)
+                <p class="lead tac">{{ $page->intro }}</p>
+            @endif
 
             @if($page->image)
                 <div class="my-2"><img src="{{ $page->mainImageUrl() }}" alt="{{ $page->title ?? null }}" class="mx-auto rounded-05"></div>
@@ -21,20 +23,30 @@
     </div>
 
     @if($subCategories->isNotEmpty())
+
         <section id="subCategories" class="tac my-3">
+
             <div class="container">
+
                 <div class="grid cols-4-2-1">
+
                     @foreach($subCategories as $category)
+
                         <div class="mt">
                             <a class="txt-lg" href="/{{ $category->route_prefix ? $category->route_prefix . '/' : '' }}{{ $category->slug }}">
                                 <img class="rounded-05" src="{{ $category->mainImageUrl() }}" alt="{{ $category->title ?? null }}">
                                 {{ $category->title }}
                             </a>
                         </div>
+
                     @endforeach
+
                 </div>
+
             </div>
+
         </section>
+
     @endif
 
 </x-gotime-app-layout>
